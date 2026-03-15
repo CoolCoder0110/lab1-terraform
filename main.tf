930eb5d470b215c67d550ed346d91d1a673aac12
Simon <144913703+CoolCoder0110@users.noreply.github.com>
2026-03-15 13:06:31 +0100

    Update Terraform state prefix for lab1

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "gcs" {
     bucket = "chas-tf-state-nissastigen"
-    prefix = "terraform/state/simon"
+    prefix = "terraform/state/simon/lab1"
   }
   required_providers {
     google = {
